481264e213e8c14535cf6f0df9977120544dc040
KostyantinSolod <76468012+KostyantinSolod@users.noreply.github.com>
2022-11-07 23:13:02 +0200

    Update and Add

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -4,15 +4,182 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Board board { get; private set; }
+    public TetrominoData data { get; private set; }
+    public Vector3Int[] cells { get; private set; } 
+    public Vector3Int position { get; private set; }
+    public int rotationIndex { get; private set; }
+    public float stepDelay = 1f;
+    public float moveDelay = 0.1f;
+    public float lockDelay = 0.5f;
+    private float stepTime;
+    private float moveTime;
+    private float lockTime;
+    public void Initialize(Board board,Vector3Int position, TetrominoData data)
     {
-        
+        this.position = position; 
+        this.data = data;
+        this.board = board;
+        rotationIndex = 0;
+        stepTime = Time.time + stepDelay;
+        moveTime = Time.time + moveDelay;
+        lockTime = 0f;
+        if (this.cells == null)
+        {
+            this.cells = new Vector3Int[data.cells.Length];
+        }
+        for (int i = 0; i < data.cells.Length; i++)
+        {
+            this.cells[i] = (Vector3Int)data.cells[i];
+        }
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        this.board.Clear(this);
+        this.lockTime += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Rotate(-1);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            Rotate(1);
+        }
+        if (Time.time > moveTime)
+        {
+            HandleMoveInputs();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HardDrop();
+        }
+        if (Time.time >= this.stepTime)
+        {
+            Step();
+        }
+        this.board.Set(this);
+    }
+    private void HandleMoveInputs()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (Move(Vector2Int.down))
+            {
+                stepTime = Time.time + stepDelay;
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Move(Vector2Int.left);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Move(Vector2Int.right);
+        }
+    }
+    private void Step()
+    {
+        this.stepTime = Time.time + this.stepDelay;
+        Move(Vector2Int.down);
+        if (this.lockTime>= this.lockDelay)
+        {
+            Lock();
+        }
+    }
+    private void Lock()
+    {
+        this.board.Set(this);
+        this.board.ClearLines();
+        this.board.SpawnPiece();
+    }
+    private void HardDrop()
+    {
+        while (Move(Vector2Int.down))
+        {
+            continue;
+        }
+        Lock();
+    }
+    private bool Move(Vector2Int translation)
+    {
+        Vector3Int newPosition = this.position;
+        newPosition.x += translation.x;
+        newPosition.y += translation.y;
+        bool valid = board.IsValidPosition(this,newPosition);
+        if (valid)
+        {
+            this.position = newPosition;
+            this.moveTime = Time.time + moveDelay;
+            this.lockTime = 0f;
+        }
+        return valid;
+    }
+    private void Rotate(int direction)
+    {
+        int originalRotation = this.rotationIndex;
+        this.rotationIndex = Wrap(this.rotationIndex + direction, 0, 4);
+        ApplyRotationMatrix(direction);
+        if (!TestWallKicks(this.rotationIndex, direction))
+        {
+            this.rotationIndex = originalRotation;
+            ApplyRotationMatrix(-direction);
+        }
+    }
+    private void ApplyRotationMatrix(int direction)
+    {
+        for (int i = 0; i < this.data.cells.Length; i++)
+        {
+            Vector3 cell = this.cells[i];
+            int x, y;
+            switch (this.data.tetromino)
+            {
+                case Tetromino.I:
+                case Tetromino.O:
+                    cell.x -= 0.5f;
+                    cell.y -= 0.5f;
+                    x = Mathf.CeilToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.CeilToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+                default:
+                    x = Mathf.RoundToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.RoundToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+
+            }
+            this.cells[i] = new Vector3Int(x, y, 0);
+        }
+    }
+    private int Wrap(int input, int min, int max)   
+    {
+        if (input<min)
+        {
+            return max - (min - input) % (max - min);
+        }
+        else
+        {
+            return min + (input - min) % (max - min);
+        }
+    }
+    private bool TestWallKicks(int rotationIndex, int rotationDirection)
+    {
+        int wallKickIndex = GetWallKickIndex(rotationIndex, rotationDirection);
+        for (int i = 0; i < this.data.wallkicks.GetLength(1); i++)
+        {
+            Vector2Int translation = this.data.wallkicks[wallKickIndex, i];
+            if (Move(translation))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    private int GetWallKickIndex(int rotationIndex, int rotationDirection)
+    {
+        int wallKickIndex = rotationIndex * 2;
+        if (rotationIndex < 0)
+        {
+            wallKickIndex--;
+        }
+        return Wrap(wallKickIndex,0,this.data.wallkicks.GetLength(0));
     }
 }
